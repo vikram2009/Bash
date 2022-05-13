da0832983277d8ede3683136abe80f6a84968368
vikram2009 <73585468+vikram2009@users.noreply.github.com>
2022-05-13 19:28:37 +0530

    v1
    run this now
    ./randomuse.sh

---
+++ randomuse.sh
@@ -0,0 +1,51 @@
+name="-> G I N Y O S <-"
+
+echo "Welcome"
+echo "Starting.."
+sleep 1
+echo "**--------------"
+sleep 1
+echo "*****-----------"
+sleep 1
+echo "********--------"
+sleep 1
+echo "***********-----"
+sleep 1
+echo "*************---"
+sleep 1
+echo "***************--"
+sleep 1
+echo "*****************"
+echo "$name"
+echo "|~~~~~~~~~~~~~~|" 
+echo "|**************|"
+echo "|``````````````|"
+echo "|--------------|"
+echo "|##############|"
+echo "\/----------- \/"
+echo " \    _       /"
+echo "  \  | __   /"
+echo "   \ |_| |  /"
+echo "    \      /"
+echo "     \    /"
+echo "      \  /"
+echo "       \/"
+echo "$name"
+echo "YOU ARE LOGGED IN AS :- $location"
+
+sleep 1
+echo "vikram2009 @github "
+sleep 1
+echo "websites.ginyos@gmail.com - email ID"
+sleep 1
+echo "Contact me :- http://ayo.so/_toxic_coder"
+sleep 1
+echo "@Ginyos-secure -> github.com"
+sleep 1
+echo "Ginyos.tk <...COMMING SOON...>"
+sleep 2
+echo "BYE..."
+sleep 1
+echo "BYE..."
+
+
