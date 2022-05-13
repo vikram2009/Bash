2d83957b948da6fa727d6ef68c58442832b5b610
vikram2009 <73585468+vikram2009@users.noreply.github.com>
2022-05-13 18:31:16 +0530

    v1

---
+++ Hello.sh
@@ -0,0 +1,23 @@
+location=$(whoami)
+
+echo "Welcome to my App"
+sleep 1
+echo "What is ur Name"
+read name 
+echo "Hi @$name"
+sleep 1
+echo "what is ur age "
+read age 
+sleep 1
+echo "YOU ARE LOGGED IN AS :- $location"
+
+sleep 1
+echo "vikram2009 @github "
+sleep 1
+echo "websites.ginyos@gmail.com - email ID"
+sleep 1
+echo "Contact me :- http://ayo.so/_toxic_coder"
+sleep 2
+echo "BYE..."
+sleep 1
+echo "BYE..."
